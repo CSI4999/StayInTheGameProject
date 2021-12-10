c774a57ac409239a829f489eb237cd6a705adb9f
Nik Bergamin <nbergamin@oakland.edu>
2021-12-10 11:15:06 -0500

    fixed App.js

--- web/stayinthegame/src/App.js
+++ web/stayinthegame/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Blog from './components/home/Blog';
 import Dashboard from './components/dashboard/Dashboard';
 import SignIn from './components/sign-in/SignIn';
 //import Table from './components/portfolio/Table'
@@ -68,6 +69,10 @@ class App extends Component {
 
               <Route exact path="/portfolio">
                 <Table></Table>
+              </Route>
+
+              <Route exact path="/home">
+                <Blog></Blog>
               </Route>
             </Switch>
           </div>
