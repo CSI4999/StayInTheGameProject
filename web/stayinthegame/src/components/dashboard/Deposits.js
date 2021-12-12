8cff8c2be673053fd665ee0a2a2ba589b4f0bcef
TQFallon3 <90359606+TQFallon3@users.noreply.github.com>
2021-12-11 21:56:51 -0500

    Update

--- web/stayinthegame/src/components/dashboard/Deposits.js
+++ web/stayinthegame/src/components/dashboard/Deposits.js
@@ -4,9 +4,7 @@ import Typography from '@mui/material/Typography';
 import Title from './Title';
 import {fetchBuySell} from '../../api/api';
 
-function preventDefault(event) {
-  event.preventDefault();
-}
+
 
 function createData(buy, sell, date) {
   return {
@@ -17,10 +15,10 @@ function createData(buy, sell, date) {
 }
 
 
-export default function Deposits() {
+export default function Deposits({ symbol }) {
   function BSH({symbol}) {
     const [open, setOpen] = React.useState(false);
-  
+    const [ticker, setTicker] = React.useState(symbol)
     const [buysellData, setBuySellData] = React.useState([])
     React.useEffect(() => {
       fetchBuySell(symbol).then(({ data }) => {
@@ -41,6 +39,9 @@ export default function Deposits() {
 
 
   return (
+    
+
+
     <React.Fragment>
       <Title>Recommendation</Title>
       <Typography component="p" variant="h4">
@@ -50,12 +51,15 @@ export default function Deposits() {
       on {new Date().toLocaleString("en-US", { month: "long" })} {new Date().toLocaleString("en-US", { day : '2-digit'})}, {new Date().getFullYear()}
       </Typography>
       <Title>Bot Recommendation</Title>
+      
       <Typography color="text.secondary" sx={{ flex: 1 }}>
         Buy: Price / Sell: Price
       </Typography>
+      
       <Typography color="text.secondary" sx={{ flex: 1 }}>
       on {new Date().toLocaleString("en-US", { month: "long" })} {new Date().toLocaleString("en-US", { day : '2-digit'})}, {new Date().getFullYear()}
       </Typography>
     </React.Fragment>
   );
+
 }
